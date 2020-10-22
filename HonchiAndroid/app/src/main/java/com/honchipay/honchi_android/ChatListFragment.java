7e1a06980eafbb3e80401b88a97f9d8b54646431
sirasatarato <sirasatarato@gmail.com>
2020-10-22 11:56:25 +0900

    Create ChatListFragment

---
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/ChatListFragment.java
@@ -0,0 +1,16 @@
+package com.honchipay.honchi_android;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChatListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat_list, container, false);
+    }
+}
