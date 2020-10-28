e873fc7ed1ab87c8097ba161c6ceba6ab91bb4cd
minhee <jwmh15@gmail.com>
2020-10-28 23:22:33 +0900

    Add files

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/sign/Fragment/SignUp_password_Fragment.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/sign/Fragment/SignUp_password_Fragment.java
@@ -1,4 +1,4 @@
-package com.honchipay.honchi_android;
+package com.honchipay.honchi_android.sign.Fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SignUp_user_info_Fragment extends Fragment {
+import com.honchipay.honchi_android.R;
+
+public class SignUp_password_Fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_user_info, container, false);
+        return inflater.inflate(R.layout.fragment_sign_up_password, container, false);
     }
 }
---
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/splash/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.honchipay.honchi_android.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.honchipay.honchi_android.R;
+import com.honchipay.honchi_android.sign.SignActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    Button login_btn;
+    Button signUp_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        login_btn = findViewById(R.id.splash_login_button);
+        signUp_btn = findViewById(R.id.splash_signUp_button);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignActivity.class);
+                intent.putExtra("splash","login");
+                startActivity(intent);
+            }
+        });
+
+        signUp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignActivity.class);
+                intent.putExtra("splash","signUp");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
