b81ecc407282cab0da7c56c7730738630240443e
sirasatarato <sirasatarato@gmail.com>
2020-10-28 11:08:13 +0900

    Move to chat folder

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/ChatListFragment.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/ChatListFragment.java
@@ -1,4 +1,4 @@
-package com.honchipay.honchi_android;
+package com.honchipay.honchi_android.chat;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.honchipay.honchi_android.R;
 
 public class ChatListFragment extends Fragment {
     @Override
--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/MessengerActivity.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/MessengerActivity.java
@@ -1,8 +1,10 @@
-package com.honchipay.honchi_android;
+package com.honchipay.honchi_android.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.honchipay.honchi_android.R;
 
 public class MessengerActivity extends AppCompatActivity {
 
