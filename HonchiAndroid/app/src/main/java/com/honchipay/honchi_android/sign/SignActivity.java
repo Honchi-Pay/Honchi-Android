bc51fed95167139bcca15254d21b0172f3ee8908
sirasatarato <sirasatarato@gmail.com>
2020-10-29 11:48:03 +0900

    Update Sign Activity code

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/sign/SignActivity.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/sign/SignActivity.java
@@ -1,32 +1,38 @@
 package com.honchipay.honchi_android.sign;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import com.honchipay.honchi_android.R;
+import com.honchipay.honchi_android.sign.Fragment.LoginFragment;
+import com.honchipay.honchi_android.sign.Fragment.SignUp_email_Fragment;
 
 public class SignActivity extends AppCompatActivity {
-    Intent intent;
-    String division;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
 
-        intent = getIntent();
-        division = intent.getExtras().getString("splash");
+        String division = getIntent().getStringExtra("splash");
+        transaction = getSupportFragmentManager().beginTransaction();
 
-        if(division == "login"){
-
-        } else if(division == "signUp"){
-
-        } else{
-            Toast.makeText(this,"올바르지 않은 요청입니다.",Toast.LENGTH_LONG).show();
+        switch (division) {
+            case "login":
+                transaction.add(R.id.sign_fragment, new LoginFragment()).commit();
+                break;
+            case "signUp":
+                transaction.add(R.id.sign_fragment, new SignUp_email_Fragment()).commit();
+                break;
+            default:
+                Toast.makeText(this,"올바르지 않은 요청입니다.",Toast.LENGTH_LONG).show();
         }
+    }
 
+    void replaceFragment(Fragment fragment) {
+        transaction.replace(R.id.sign_fragment, fragment).commit();
     }
 }
