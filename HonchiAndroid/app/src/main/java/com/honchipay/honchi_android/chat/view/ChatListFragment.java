a1711574e688ca7e6d1cbda3719679b9c9453f63
sirasatarato <sirasatarato@gmail.com>
2020-11-02 20:23:48 +0900

    Add okhttp websocket

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatListFragment.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatListFragment.java
@@ -2,6 +2,7 @@ package com.honchipay.honchi_android.chat.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,5 +15,12 @@ public class ChatListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chat_list, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
     }
 }
