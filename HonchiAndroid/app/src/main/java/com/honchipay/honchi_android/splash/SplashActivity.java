0fb91c9343740c404e9b57c77f54b8bec11ad120
sirasatarato <sirasatarato@gmail.com>
2020-11-02 19:16:25 +0900

    Add permission

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/splash/SplashActivity.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/splash/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
+
 import com.honchipay.honchi_android.R;
 import com.honchipay.honchi_android.sign.SignActivity;
 
@@ -43,7 +45,9 @@ public class SplashActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void checkPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                && ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
             new AlertDialog.Builder(this)
                     .setTitle("알림")
                     .setMessage("권한이 거부되었습니다.\n사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
@@ -58,7 +62,7 @@ public class SplashActivity extends AppCompatActivity implements View.OnClickLis
         } else {
             ActivityCompat.requestPermissions(
                     this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
                     PERMISSION_CODE
             );
         }
