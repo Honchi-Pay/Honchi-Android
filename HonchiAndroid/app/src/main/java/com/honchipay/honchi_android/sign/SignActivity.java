f762e631dab19cc9c2efbbb3a88e7228b80cceb1
sirasatarato <sirasatarato@gmail.com>
2020-11-04 18:06:30 +0900

    Fixed "commit already call"

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/sign/SignActivity.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/sign/SignActivity.java
@@ -3,22 +3,33 @@ package com.honchipay.honchi_android.sign;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+
 import com.honchipay.honchi_android.R;
 import com.honchipay.honchi_android.sign.Fragment.LoginFragment;
 import com.honchipay.honchi_android.sign.Fragment.SignUpEmailFragment;
+import com.honchipay.honchi_android.util.SharedPreferencesManager;
 
 public class SignActivity extends AppCompatActivity {
-    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
 
+        SharedPreferencesManager sharedPreferences = SharedPreferencesManager.getInstance();
+        if (sharedPreferences != null) {
+            String token = sharedPreferences.getAccessToken();
+            if (!token.equals("")) Log.e("SignActivity", token);
+            String refresh = sharedPreferences.getRefreshToken();
+            if (!refresh.equals("")) Log.e("SignActivity", refresh);
+        }
+
         String division = getIntent().getStringExtra("splash");
-        transaction = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         switch (division) {
             case "login":
@@ -28,11 +39,13 @@ public class SignActivity extends AppCompatActivity {
                 transaction.add(R.id.sign_fragment, new SignUpEmailFragment()).commit();
                 break;
             default:
-                Toast.makeText(this,"올바르지 않은 요청입니다.",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "올바르지 않은 요청입니다.", Toast.LENGTH_LONG).show();
         }
     }
 
     public void replaceFragment(Fragment fragment) {
-        transaction.replace(R.id.sign_fragment, fragment).commit();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.sign_fragment, fragment);
+        transaction.commit();
     }
 }
