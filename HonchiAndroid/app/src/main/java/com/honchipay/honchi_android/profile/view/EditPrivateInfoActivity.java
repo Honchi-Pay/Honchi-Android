8acfacd3a106c5891ff4e57f57a500e09a1dbe85
sirasatarato <sirasatarato@gmail.com>
2020-11-05 17:10:34 +0900

    Create EditPrivateInfo Activity

---
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/profile/view/EditPrivateInfoActivity.java
@@ -0,0 +1,16 @@
+package com.honchipay.honchi_android.profile.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.honchipay.honchi_android.R;
+
+public class EditPrivateInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_private_info);
+    }
+}
