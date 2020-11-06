25cf7c8bff17dbe81f0cb2998db9fd209d99febd
minhee <jwmh15@gmail.com>
2020-11-06 23:01:17 +0900

    Add Network

---
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/network/HonchipayApi.java
@@ -0,0 +1,12 @@
+package com.honchipay.honchi_android.network;
+
+import com.honchipay.honchi_android.purchaseHistory.Model.BuyListDetailItem;
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.Response;
+import retrofit2.http.Field;
+import retrofit2.http.GET;
+
+public interface HonchipayApi {
+    @GET("/post/buyList")
+    Single<Response<BuyListDetailItem>> buyList(@Field("Authorization") String authorization);
+}
