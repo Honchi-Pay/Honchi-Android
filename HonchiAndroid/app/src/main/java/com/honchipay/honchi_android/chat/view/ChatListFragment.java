086693091d184598ae8e9f05c141b85e9f1e06fe
sirasatarato <sirasatarato@gmail.com>
2020-11-11 10:52:32 +0900

    Create ViewModel

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatListFragment.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,11 @@ import android.view.ViewGroup;
 
 import com.honchipay.honchi_android.R;
 import com.honchipay.honchi_android.chat.HonchiPaySocket;
+import com.honchipay.honchi_android.chat.viewModel.ChatViewModel;
 
 public class ChatListFragment extends Fragment {
+    ChatViewModel chatViewModel;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chat_list, container, false);
@@ -22,6 +26,7 @@ public class ChatListFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        chatViewModel = new ViewModelProvider(this).get(ChatViewModel.class);
         HonchiPaySocket.getInstance().connectSocket();
     }
 }
