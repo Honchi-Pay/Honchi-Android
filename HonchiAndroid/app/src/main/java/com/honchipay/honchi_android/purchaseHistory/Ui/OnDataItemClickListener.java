efbd681754de29e807bf5e768b43702ba5b721de
minhee <jwmh15@gmail.com>
2020-11-12 11:04:41 +0900

    Delete files

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/purchaseHistory/Ui/OnDataItemClickListener.java
+++
@@ -1,5 +0,0 @@
-package com.honchipay.honchi_android.purchaseHistory.Ui;
-
-public interface OnDataItemClickListener {
-    public void onItemClick(BuyListAdapter.ViewHolder)
-}
--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/purchaseHistory/ViewModel/PurchaseHistoryViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.honchipay.honchi_android.purchaseHistory.ViewModel;
-
-import androidx.lifecycle.ViewModel;
-
-public class PurchaseHistoryViewModel extends ViewModel {
-}
