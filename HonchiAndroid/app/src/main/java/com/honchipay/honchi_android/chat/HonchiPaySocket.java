e75e39c0a16123d55586a932b9d06146eebed3a3
sirasatarato <sirasatarato@gmail.com>
2020-11-13 10:10:07 +0900

    Delete StompProtocolAndroid

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/HonchiPaySocket.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/HonchiPaySocket.java
@@ -1,13 +1,9 @@
 package com.honchipay.honchi_android.chat;
-import ua.naiksoftware.stomp.Stomp;
-import ua.naiksoftware.stomp.StompClient;
 
 public class HonchiPaySocket {
     private static HonchiPaySocket single_instance = null;
-    public static StompClient stompClient;
 
     private HonchiPaySocket() {}
-
     public static HonchiPaySocket getInstance() {
         if (single_instance == null) {
             single_instance = new HonchiPaySocket();
@@ -16,8 +12,5 @@ public class HonchiPaySocket {
         return single_instance;
     }
 
-    public void connectSocket() {
-        stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "");
-        stompClient.connect();
-    }
+    
 }
