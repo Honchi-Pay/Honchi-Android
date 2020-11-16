8ce720788f7842205b7ff06de135880770626542
sirasatarato <sirasatarato@gmail.com>
2020-11-16 20:21:10 +0900

    Make HonchiPaySocket by socket.io

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/HonchiPaySocket.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/HonchiPaySocket.java
@@ -1,9 +1,21 @@
 package com.honchipay.honchi_android.chat;
 
-public class HonchiPaySocket {
-    private static HonchiPaySocket single_instance = null;
+import android.util.Log;
 
-    private HonchiPaySocket() {}
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.URISyntaxException;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class HonchiPaySocket {
+    private static final String TAG = HonchiPaySocket.class.getSimpleName();
+    private static final String SERVER_URL = "";
+    private boolean isConnected = false;
+    private static HonchiPaySocket single_instance = null;
+    public Socket socket;
+
     public static HonchiPaySocket getInstance() {
         if (single_instance == null) {
             single_instance = new HonchiPaySocket();
@@ -12,5 +24,33 @@ public class HonchiPaySocket {
         return single_instance;
     }
 
-    
+    public void connect() {
+        try {
+            socket = IO.socket(SERVER_URL);
+            socket.connect();
+
+            isConnected = true;
+            Log.e(TAG, "success connected");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+
+            isConnected = false;
+        }
+    }
+
+    public void sendParticipants(String[] participants) {
+        if (isConnected) {
+            socket.emit("", (Object) participants);
+        }
+    }
+
+    public void joinIntoRoom(String roomId) {
+        socket.emit("join", roomId);
+    }
+
+    public void disConnect() {
+        if (isConnected) {
+            socket.disconnect();
+        }
+    }
 }
