e2601159fb50c9c2d53a87473f2f4209b563cf78
sirasatarato <sirasatarato@gmail.com>
2020-11-16 20:22:06 +0900

    Update BaseViewModel

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/base/BaseViewModel.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/base/BaseViewModel.java
@@ -7,14 +7,25 @@ import androidx.lifecycle.ViewModel;
 
 import com.bumptech.glide.Glide;
 
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class BaseViewModel extends ViewModel {
     protected CompositeDisposable compositeDisposable = new CompositeDisposable();
 
-    public void addDisposable(Disposable disposable) {
+    protected void addDisposable(Disposable disposable) {
         compositeDisposable.add(disposable);
+    }
+
+    protected <T> void addSingle(Single<T> single, DisposableSingleObserver<T> observer) {
+        addDisposable(single
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(observer));
     }
 
     @Override
--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/viewModel/ChatViewModel.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/viewModel/ChatViewModel.java
@@ -1,46 +1,46 @@
 package com.honchipay.honchi_android.chat.viewModel;
 
-import android.util.Log;
-
 import androidx.lifecycle.MutableLiveData;
 
 import com.honchipay.honchi_android.base.BaseViewModel;
 import com.honchipay.honchi_android.chat.model.ChatListItem;
 import com.honchipay.honchi_android.chat.model.ChatRepository;
-import com.honchipay.honchi_android.sign.Data.TokenResponseData;
-import com.honchipay.honchi_android.util.SharedPreferencesManager;
-
-import org.jetbrains.annotations.NotNull;
+import com.honchipay.honchi_android.util.CustomDisposableSingleObserver;
 
 import java.util.List;
+import java.util.function.Function;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.observers.DisposableSingleObserver;
-import io.reactivex.schedulers.Schedulers;
 import retrofit2.Response;
 
 public class ChatViewModel extends BaseViewModel {
     private final ChatRepository repository = new ChatRepository();
     public MutableLiveData<List<ChatListItem>> chatRoomListLiveData = new MutableLiveData<>();
+    private final String TAG = ChatViewModel.class.getSimpleName();
 
     public void getParticipatingChatRooms() {
-        addDisposable(repository.getChatList()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<Response<List<ChatListItem>>>() {
-                    @Override
-                    public void onSuccess(@NonNull Response<List<ChatListItem>> chatListItemResponse) {
-                        if (chatListItemResponse.isSuccessful() && chatListItemResponse.code() == 200) {
-                            chatRoomListLiveData.postValue(chatListItemResponse.body());
-                        }
-                    }
+        DisposableSingleObserver<Response<List<ChatListItem>>> chatListItemObserver =
+                new CustomDisposableSingleObserver<Response<List<ChatListItem>>>(TAG) {
+            @Override
+            public void onSuccess(@NonNull Response<List<ChatListItem>> listResponse) {
+                if (listResponse.isSuccessful() && listResponse.code() == 200) {
+                    chatRoomListLiveData.postValue(listResponse.body());
+                }
+            }
+        };
 
+        addSingle(repository.getChatList(), chatListItemObserver);
+    }
+
+    public void changeChatRoomTitle(String title) {
+        DisposableSingleObserver<Response<Void>> roomTitleObserver =
+                new CustomDisposableSingleObserver<Response<Void>>(TAG) {
                     @Override
-                    public void onError(@NotNull Throwable e) {
-                        Log.e("ChatViewModel", e.getMessage());
+                    public void onSuccess(@NonNull Response<Void> listResponse) {
                     }
-                })
-        );
+                };
+
+        addSingle(repository.changeRoomTitle(title), roomTitleObserver);
     }
 }
