ec31dcf5832ae91ac94019f5bf2f59f79249a7c4
sirasatarato <sirasatarato@gmail.com>
2020-11-19 17:01:07 +0900

    Update code to add wrapping single

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/model/ChatRepository.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/model/ChatRepository.java
@@ -6,14 +6,18 @@ import com.honchipay.honchi_android.network.HonchipayConnector;
 import java.util.List;
 
 import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Response;
 
 public class ChatRepository extends BaseRepository {
-    public Single<Response<List<ChatRoomItem>>> getChatList() {
-        return HonchipayConnector.getInstance().getApi().getChatList(token);
+    public Disposable changeRoomTitle(String title, DisposableSingleObserver<Response<Void>> roomTitleObserver) {
+        return wrappingSingle(HonchipayConnector.getInstance().getApi().updateChatRoomTitle(title), roomTitleObserver);
     }
 
-    public Single<Response<Void>> changeRoomTitle(String title) {
-        return HonchipayConnector.getInstance().getApi().updateChatRoomTitle(title);
+    public Disposable getChatRooms(DisposableSingleObserver<Response<List<ChatRoomItem>>> chatRoomsObserver) {
+        return wrappingSingle(HonchipayConnector.getInstance().getApi().getChatRooms(token), chatRoomsObserver);
     }
 }
