dde7a5d4cd68e30197b5e18ae373f327765c8a93
minhee <jwmh15@gmail.com>
2020-11-22 23:14:22 +0900

    Add data class

---
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/home/Data/getPost.java
@@ -0,0 +1,57 @@
+package com.honchipay.honchi_android.home.Data;
+
+public class getPost {
+    int postId;
+    String title;
+    String writer;
+    String image;
+    String createAt;
+
+    public getPost(int postId, String title, String writer, String image, String createAt) {
+        this.postId = postId;
+        this.title = title;
+        this.writer = writer;
+        this.image = image;
+        this.createAt = createAt;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+}
---
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/home/Data/newPost.java
@@ -0,0 +1,87 @@
+package com.honchipay.honchi_android.home.Data;
+
+public class newPost {
+    int postId;
+    String title;
+    String writer;
+    String item;
+    Double lat;
+    Double lon;
+    String createdAt;
+    String image;
+
+    public newPost(int postId, String title, String writer, String item, Double lat, Double lon, String createdAt, String image) {
+        this.postId = postId;
+        this.title = title;
+        this.writer = writer;
+        this.item = item;
+        this.lat = lat;
+        this.lon = lon;
+        this.createdAt = createdAt;
+        this.image = image;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
