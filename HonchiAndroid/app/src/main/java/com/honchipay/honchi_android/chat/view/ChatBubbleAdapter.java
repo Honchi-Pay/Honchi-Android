3351817f26e10390670fed1ac7a11d72a0f07967
sirasatarato <sirasatarato@gmail.com>
2020-11-29 22:36:15 +0900

    Division ChatBubble Classes

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatBubbleAdapter.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatBubbleAdapter.java
@@ -1,29 +1,20 @@
 package com.honchipay.honchi_android.chat.view;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.honchipay.honchi_android.R;
-import com.honchipay.honchi_android.chat.model.ChattingContent;
+import com.honchipay.honchi_android.chat.model.MessageResponse;
 
 import java.util.List;
 
-public class ChatBubbleAdapter extends RecyclerView.Adapter<ChatBubbleAdapter.ChatBubbleViewHolder> {
-    List<ChattingContent> chattingContentList;
+public class ChatBubbleAdapter extends RecyclerView.Adapter<ChatBubbleViewHolder> {
+    List<MessageResponse> chattingContentList;
 
-    ChatBubbleAdapter(List<ChattingContent> contentList) {
+    ChatBubbleAdapter(List<MessageResponse> contentList) {
         chattingContentList = contentList;
     }
 
@@ -35,12 +26,8 @@ public class ChatBubbleAdapter extends RecyclerView.Adapter<ChatBubbleAdapter.Ch
 
     @Override
     public void onBindViewHolder(@NonNull ChatBubbleViewHolder holder, int position) {
-        ChattingContent chattingContent = chattingContentList.get(position);
-//        if (chattingContent.getContent() != null) {
-//            holder.makeTextView(chattingContent.getContent());
-//        } else {
-//            holder.makeImageView(chattingContent.getImg());
-//        }
+        MessageResponse messageResponse = chattingContentList.get(position);
+        holder.makeTextView(messageResponse);
     }
 
     @Override
@@ -48,67 +35,14 @@ public class ChatBubbleAdapter extends RecyclerView.Adapter<ChatBubbleAdapter.Ch
         return chattingContentList.size();
     }
 
-    public void addMessage(String message) {
-        ChattingContent chattingContent = new ChattingContent();
-        chattingContent.setContent(message);
-        addToContentList(chattingContent);
-    }
-
-    public void addImage(String image) {
-        ChattingContent chattingContent = new ChattingContent();
-        chattingContent.setImg(image);
-        addToContentList(chattingContent);
-    }
-
-    private void addToContentList(ChattingContent chattingContent) {
-        chattingContentList.add(chattingContentList.size(), chattingContent);
+    public void setAllMessages(List<MessageResponse> messages) {
+        chattingContentList.clear();
+        chattingContentList.addAll(messages);
         notifyDataSetChanged();
     }
 
-    static class ChatBubbleViewHolder extends RecyclerView.ViewHolder {
-        public ChatBubbleViewHolder(View itemView) {
-            super(itemView);
-        }
-
-        public void makeTextView(String message, boolean isMine) {
-            TextView textView = new TextView(itemView.getContext());
-            textView.setText(message);
-            textView.setBackground(ContextCompat.getDrawable(itemView.getContext(), R.drawable.background_horizontal_bar));
-
-            LinearLayout linearLayout = new LinearLayout(itemView.getContext());
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-            if (isMine) {
-                textView.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#79A5DF")));
-                layoutParams.gravity = Gravity.END;
-            } else {
-                textView.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#E4E4E4")));
-                layoutParams.gravity = Gravity.START;
-            }
-
-            linearLayout.setLayoutParams(layoutParams);
-            linearLayout.addView(textView);
-            ((LinearLayout) itemView.findViewById(R.id.item_bubble_own_layout)).addView(linearLayout);
-        }
-
-        public void makeImageView(String imageUrl, boolean isMine) {
-            ImageView imageView = new ImageView(itemView.getContext());
-            imageView.setBackground(ContextCompat.getDrawable(itemView.getContext(), R.drawable.background_horizontal_bar));
-            imageView.setClipToOutline(true);
-
-            Glide.with(imageView.getContext()).load(imageUrl).into(imageView);
-            LinearLayout linearLayout = new LinearLayout(itemView.getContext());
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-            if (isMine) {
-                layoutParams.gravity = Gravity.END;
-            } else {
-                layoutParams.gravity = Gravity.START;
-            }
-
-            linearLayout.setLayoutParams(layoutParams);
-            linearLayout.addView(imageView);
-            ((LinearLayout) itemView.findViewById(R.id.item_bubble_own_layout)).addView(linearLayout);
-        }
+    public void addMessage(MessageResponse message) {
+        chattingContentList.add(chattingContentList.size(), message);
+        notifyDataSetChanged();
     }
 }
---
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatBubbleViewHolder.java
@@ -0,0 +1,58 @@
+package com.honchipay.honchi_android.chat.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.honchipay.honchi_android.R;
+import com.honchipay.honchi_android.chat.model.MessageResponse;
+
+class ChatBubbleViewHolder extends RecyclerView.ViewHolder {
+    LinearLayout linearLayout;
+
+    public ChatBubbleViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    public void makeTextView(MessageResponse messageResponse) {
+        if (messageResponse.isMine()) {
+            makeBubbleView(R.layout.layout_right_bubble_chat, messageResponse);
+        } else {
+            makeBubbleView(R.layout.layout_left_bubble_chat, messageResponse);
+        }
+    }
+
+    private void makeBubbleView(int directionLayoutResourceId, MessageResponse messageResponse) {
+        linearLayout = new LinearLayout(itemView.getContext());
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        linearLayout.setLayoutParams(layoutParams);
+        linearLayout.addView(LayoutInflater.from(itemView.getContext()).inflate(directionLayoutResourceId, linearLayout, false));
+        setValuesByDirection(messageResponse);
+        ((LinearLayout) itemView.findViewById(R.id.item_bubble_own_layout)).addView(linearLayout);
+    }
+
+    private void setValuesByDirection(MessageResponse messageResponse) {
+        if (messageResponse.isMine()) {
+            setMessageByIsDelete(messageResponse.isDelete(), messageResponse.getMessage(), R.id.right_bubble_message_textView);
+            ((TextView) linearLayout.findViewById(R.id.right_bubble_userName_textView)).setText(messageResponse.getNickName());
+            ((TextView) linearLayout.findViewById(R.id.right_bubble_time_textView)).setText(messageResponse.getTime().toString());
+            ((TextView) linearLayout.findViewById(R.id.right_bubble_readCount_textView)).setText(messageResponse.getReadCount());
+        } else {
+            setMessageByIsDelete(messageResponse.isDelete(), messageResponse.getMessage(), R.id.left_bubble_message_textView);
+            ((TextView) linearLayout.findViewById(R.id.left_bubble_userName_textView)).setText(messageResponse.getNickName());
+            ((TextView) linearLayout.findViewById(R.id.left_bubble_time_textView)).setText(messageResponse.getTime().toString());
+            ((TextView) linearLayout.findViewById(R.id.left_bubble_readCount_textView)).setText(messageResponse.getReadCount());
+        }
+    }
+
+    private void setMessageByIsDelete(boolean isDelete, String message, int bubbleMessageTextViewResourceId) {
+        if (isDelete) {
+            ((TextView) linearLayout.findViewById(bubbleMessageTextViewResourceId)).setText("삭제된 메시지입니다.");
+        } else {
+            ((TextView) linearLayout.findViewById(bubbleMessageTextViewResourceId)).setText(message);
+        }
+    }
+}
