7bd330a8b83566456d19c802cc7916697b6015f7
sirasatarato <sirasatarato@gmail.com>
2020-11-30 09:27:55 +0900

    Make action to delete message

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatBubbleAdapter.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/view/ChatBubbleAdapter.java
@@ -4,18 +4,22 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.honchipay.honchi_android.R;
 import com.honchipay.honchi_android.chat.model.MessageResponse;
+import com.honchipay.honchi_android.chat.viewModel.ChatViewModel;
 
 import java.util.List;
 
 public class ChatBubbleAdapter extends RecyclerView.Adapter<ChatBubbleViewHolder> {
     List<MessageResponse> chattingContentList;
+    ChatViewModel chatViewModel;
 
-    ChatBubbleAdapter(List<MessageResponse> contentList) {
+    ChatBubbleAdapter(List<MessageResponse> contentList, ChatViewModel chatViewModel) {
         chattingContentList = contentList;
+        this.chatViewModel = chatViewModel;
     }
 
     @NonNull
@@ -28,6 +32,20 @@ public class ChatBubbleAdapter extends RecyclerView.Adapter<ChatBubbleViewHolder
     public void onBindViewHolder(@NonNull ChatBubbleViewHolder holder, int position) {
         MessageResponse messageResponse = chattingContentList.get(position);
         holder.makeTextView(messageResponse);
+
+        holder.itemView.setOnLongClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setTitle("메시지 삭제하시겠습니까?");
+            builder.setPositiveButton("네", (dialog, id) -> {
+                chatViewModel.deleteMessage(messageResponse.getMessageId());
+                dialog.dismiss();
+            });
+            builder.setNegativeButton("아니요", (dialog, id) -> dialog.dismiss());
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+            return true;
+        });
     }
 
     @Override
