4db0a0193cf0199269d930b0c7020b9652351e48
minhee <jwmh15@gmail.com>
2020-12-05 01:36:23 +0900

    Add bottomNavigation

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/home/Ui/homeActivity.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/home/Ui/homeActivity.java
@@ -1,18 +1,24 @@
 package com.honchipay.honchi_android.home.Ui;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.honchipay.honchi_android.R;
 import com.honchipay.honchi_android.databinding.ActivityHomeBinding;
-
-import java.util.ArrayList;
+import com.honchipay.honchi_android.writing.writingFragment;
 
 public class homeActivity extends AppCompatActivity {
     private ActivityHomeBinding binding;
-    private homeAdapter adapter;
-    private ArrayList foodList = new ArrayList<>();
+    FragmentManager fm = getSupportFragmentManager();
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,46 @@ public class homeActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
 
+        transaction = fm.beginTransaction();
+        transaction.replace(R.id.home_fragment, new writingFragment());
+        transaction.commit();
+
+        binding.homeNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navi_home: {
+                        transaction.replace(R.id.home_fragment, new homeFragment()).commit();
+                        break;
+                    }
+
+                    case R.id.navi_buylist: {
+                        break;
+                    }
+
+                    case R.id.navi_message: {
+                        break;
+                    }
+
+                    case R.id.navi_profile: {
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_home, menu);
+        return true;
+    }
 
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //case R.id.homeBar_back_button
+        }
+    }*/
 }
