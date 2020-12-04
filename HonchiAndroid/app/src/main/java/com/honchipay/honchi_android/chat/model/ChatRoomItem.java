ba389bc58b5cac5be5a974530aacf5d520dc1467
sirasatarato <sirasatarato@gmail.com>
2020-12-04 09:22:39 +0900

    Apply Builder Pattern

--- HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/model/ChatRoomItem.java
+++ HonchiAndroid/app/src/main/java/com/honchipay/honchi_android/chat/model/ChatRoomItem.java
@@ -1,13 +1,54 @@
 package com.honchipay.honchi_android.chat.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatRoomItem implements Serializable {
     private String roomId;
     private String title;
     private int people;
     private String message;
-    private String images;
+    private ArrayList<String> images;
+
+    public static class Builder {
+        private String roomId;
+        private String title;
+        private int people = 1;
+        private String message = "";
+        private ArrayList<String> images = new ArrayList<>();
+
+        public Builder(String roomId, String title) {
+            this.roomId = roomId;
+            this.title = title;
+        }
+
+        public Builder people(int people) {
+            this.people = people;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+        public Builder images(ArrayList<String> images) {
+            this.images = images;
+            return this;
+        }
+
+        public ChatRoomItem build() {
+            return new ChatRoomItem(this);
+        }
+    }
+
+    private ChatRoomItem(Builder builder) {
+        roomId = builder.roomId;
+        title = builder.title;
+        people = builder.people;
+        message = builder.message;
+        images = builder.images;
+    }
 
     public String getRoomId() {
         return roomId;
@@ -42,10 +83,14 @@ public class ChatRoomItem implements Serializable {
     }
 
     public String getImages() {
-        return images;
+        if (images.size() == 0) {
+            return null;
+        } else {
+            return images.get(0);
+        }
     }
 
-    public void setImages(String images) {
+    public void setImages(ArrayList<String> images) {
         this.images = images;
     }
 }
